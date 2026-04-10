862f21dc0bcc5d3ab1699ac6844b0d9ab269cbf7
Konik Rahman <konikrahman06@gmail.com>
2026-04-10 21:24:28 +0300

    date: 10/04/26: first python program with first comment

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+#date :- 10.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: print the my name (or your name).
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# End
