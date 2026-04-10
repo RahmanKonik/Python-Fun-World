4c185eb92788dadc905e6ce63c3b392525b8111f
Konik Rahman <konikrahman06@gmail.com>
2026-04-10 22:13:48 +0300

    date: 10/04/26: Python program 01: find a patient by his or her name and age, is he or she old or new patient.

---
+++ FindThePatientByAgeAndName.py
@@ -0,0 +1,25 @@
+
+#date :- 10.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+# Python program 01: find a patient by his or her name and age, is he or she old or new patient.
+
+name = "Smith John"
+age = 25
+
+patient_name = input( "Enter the patient name :")
+patient_age = int(input ( "Enter the patient age :")) ## input a integer number or convert it into a integer number after user inout it.
+
+if (patient_name == name and patient_age == age ):
+    print( "He is our old patient")
+else:
+    print( "He is under of 30 year old and a new patient")
+
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# End
