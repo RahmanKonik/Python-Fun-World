bcd1fda84d035d39ff7ba53d30a65aaa86062164
Konik Rahman <konikrahman06@gmail.com>
2026-04-11 23:27:20 +0300

    date: 11/04/26: Python program 02: Find and print out a specific word by using a index number from a sentence.

---
+++ PrintSomeSpecificWordFromASentence.py
@@ -0,0 +1,30 @@
+
+#date :- 11.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+# python program 02: Find and print out a specific word by using a index number from a sentence.
+
+sentence = "Python logical operators are used to combine or modify conditions"
+
+# index = 0,
+
+print(sentence[0]) # it will be print only 'p'
+
+# index = -1
+
+print(sentence[-1]) # it will be print only 's'
+
+# index = [0:6]
+
+print(sentence[0:6]) # it will be print 'python' word.
+
+# as same we can print any word or letter from a sentence.
+
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ==== End ====
