908b4e140b0c32943d1c1cd53cb347d191f64635
Konik Rahman <konikrahman06@gmail.com>
2026-04-15 23:45:55 +0300

    date: 15/04/26: Python program 04: print a statement by using the if statement, is it hot or cold or lovely weather for going out for walk.

---
+++ paymentStatementByGoodCredit.py
@@ -0,0 +1,22 @@
+
+#date :- 15.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: print a statement is customer has good credit, he will be get a 10% or 20% down payment in 1M house price payment.
+
+price = 1000000
+has_customer_good_credit = False
+
+if has_customer_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print (f"Down payment ${down_payment}")
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# End
