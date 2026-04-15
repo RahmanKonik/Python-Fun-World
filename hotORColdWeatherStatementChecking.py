b254d9e4a5bb200e4d6b72996fa71b0fc0518f44
Konik Rahman <konikrahman06@gmail.com>
2026-04-15 23:18:43 +0300

    date: 15/04/26: Python program 03: print a statement by using the if statement, is it hot or cold or lovely weather for going out for walk.

---
+++ hotORColdWeatherStatementChecking.py
@@ -0,0 +1,31 @@
+
+#date :- 15.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: print a statement by using the if statement, is it hot or cold or lovely weather for going out for walk.
+
+# creating  two variable.
+
+is_hot = True
+is_cold = False
+
+# getting the different value, then change the boolean value, true true, or true false or false true or false false
+
+if is_hot:
+    print("It's a hot day.")
+    print("Drink plenty of water.")
+elif is_cold:
+    print("It's cold day.")
+    print("Wear some warm clothes.")
+else:
+    print("It's lovely day for walking with your partner!")
+print("Enjoy your day!")
+
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# End
