74f677f2295bf3f528b7f1275db5b3eb3e1c3ec5
Konik Rahman <konikrahman06@gmail.com>
2026-04-15 23:47:28 +0300

    date: 15/04/26: Python program 05: print a statement is customer has good credit, he or she will be get a 10% or 20% down payment in 1M house price payment.

--- paymentStatementByGoodCredit.py
+++ paymentStatementByGoodCredit.py
@@ -3,7 +3,7 @@
 #Author :- Konik Rahman
 # Email:- konikrahman06@gmail.com
 
-#First python program: print a statement is customer has good credit, he will be get a 10% or 20% down payment in 1M house price payment.
+#First python program: print a statement is customer has good credit, he or she will be get a 10% or 20% down payment in 1M house price payment.
 
 price = 1000000
 has_customer_good_credit = False
