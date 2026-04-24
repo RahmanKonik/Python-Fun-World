f6b8a4aa1a3f8f2f1bc29f211d526ba50d11a2e7
Konik Rahman <konikrahman06@gmail.com>
2026-04-24 22:00:06 +0300

    24/04/26; python program 11: calculatiing the shopping items price

---
+++ shopping_items_prices_calculating.py
@@ -0,0 +1,24 @@
+
+#date :- 24.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: Calculating the shopping items price.
+
+# variables.
+
+total_price = 0
+items_price_array = [20, 30, 40, 50, 20]
+
+# using the for loop.
+
+for items in items_price_array:
+    total_price = total_price + items
+print(f" Total prices {total_price}, thank you for shopping!")
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ===End===
