24c4a317eb0fd3dc4d29353a5ea24dc564bd1787
Konik Rahman <konikrahman06@gmail.com>
2026-04-24 22:21:38 +0300

    24/4/26; program 12: print the alpabet using star and array values

---
+++ print_number_of_star_depend_on_array.py
@@ -0,0 +1,19 @@
+
+#date :- 24.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: print the star depend on array values.
+
+values = [5, 2, 5, 2, 2]
+
+for star in values:
+    print("x"*star)
+
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ===End===
