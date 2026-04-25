f1cabb8766b0c3ef159b167e1d5d9cbe09420478
Konik Rahman <konikrahman06@gmail.com>
2026-04-25 23:49:58 +0300

    25/5/26: program 12: updated version- find also a smallest number in that list

--- find_a_largest_Number_from_a_list.py
+++ find_a_largest_Number_from_a_list.py
@@ -7,14 +7,24 @@
 
 #variable.
 
-list_number = [12, 20, 30, 50, 62]
+list_number = [12, 20, 3, 50, 62]
 largest_number = 0
 # or largest_number = list_number[0] # which is list_number[0] = 12 which value took from the list of number variable.
+
+# largest number.
 
 for largest in list_number:
     if largest > largest_number:
         largest_number = largest
 print(f"Largest number is {largest_number} ")
+
+# smallest number.
+smallest_number = list_number[0]
+
+for smallest in list_number:
+    if smallest < smallest_number:
+        smallest_number = smallest
+print(f"Smallest number is {smallest_number} ")
 
 
 print("========@@@@@=======")
