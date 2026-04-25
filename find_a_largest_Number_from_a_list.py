d9a9887e7d59e861e4dce21d6119d24e7121f625
Konik Rahman <konikrahman06@gmail.com>
2026-04-25 23:45:35 +0300

    25/5/26: program 12: find a largest number from a list

---
+++ find_a_largest_Number_from_a_list.py
@@ -0,0 +1,25 @@
+
+#date :- 10.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: find a largest number from a list.
+
+#variable.
+
+list_number = [12, 20, 30, 50, 62]
+largest_number = 0
+# or largest_number = list_number[0] # which is list_number[0] = 12 which value took from the list of number variable.
+
+for largest in list_number:
+    if largest > largest_number:
+        largest_number = largest
+print(f"Largest number is {largest_number} ")
+
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ===End===
