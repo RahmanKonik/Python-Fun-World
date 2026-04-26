5ea980eec5ab0936945df3638a4d8ce7ccb9f794
Konik Rahman <konikrahman06@gmail.com>
2026-04-26 22:52:00 +0300

    26/4/26: program 12: removing the all the duplicate number from a list

---
+++ removing_duplicate_number_from_aList.py
@@ -0,0 +1,23 @@
+
+#date :- 26.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: removing the all duplicate number from a list.
+
+list_item = [1, 2, 3, 4, 1, 2, 3, 6, 6]
+
+unique_list = []
+
+for number in list_item:
+
+    if number not in unique_list:
+        unique_list.append(number)
+print(f" Unique list is {unique_list}")
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ===End===
