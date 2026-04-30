5a919aef7597c13cead17c0a3118497082b69436
Konik Rahman <konikrahman06@gmail.com>
2026-04-30 22:53:11 +0300

    30/4/26, program number 15: print a greeting message with using the function call

---
+++ greeting_message_in_function.py
@@ -0,0 +1,20 @@
+
+#date :- 30.04.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+
+#First python program: print a greeting message with using function call.
+
+def greeting_message():
+    print("Hi,there.")
+    print("Welcome to the finland!")
+print("Start converation, ")
+greeting_message()
+print("Finish, Thanks!")
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ===End===
