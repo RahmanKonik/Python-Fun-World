5b7a6f9c81038b09bd3793faf2d8d4063f0df7b7
Konik Rahman <konikrahman06@gmail.com>
2026-05-14 23:24:34 +0300

    14/5/2026: find a number is even or odd , what number is user input

---
+++ find_the_even_or_odd_number.py
@@ -0,0 +1,21 @@
+
+#date :- 14.05.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+#===================================
+
+#First python program: find a the even or odd number  what number is user input.
+
+user_number = int( input("Enter a number: "))
+
+if user_number % 2 == 0:
+    print(f" The {user_number} is even number.")
+else:
+    print(f" The {user_number} is odd number.")
+
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ===End===
