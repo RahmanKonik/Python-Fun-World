1e207ff8ac887face35fc979bb5cf4559ea10afd
Konik Rahman <konikrahman06@gmail.com>
2026-05-15 22:46:28 +0300

    15/5/26: nothing change, just updated some style in comment

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,14 @@
+
+#=================================
 #date :- 10.04.2026
 #Author :- Konik Rahman
 # Email:- konikrahman06@gmail.com
+#=================================
 
 #First python program: print the my name (or your name).
+
+
+#=================================
 print("========@@@@@=======")
 print("     Konik Rahman")
 print("========@@@@@=======")
