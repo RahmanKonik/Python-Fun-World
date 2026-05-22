4ccd856ed223e7a8bad794a733c57946c3e73ef3
Konik Rahman <konikrahman06@gmail.com>
2026-05-22 23:57:00 +0300

    22/5/26: updated the shopping bill system

--- Python_Fun_Project/shopping-bill-system.py
+++ Python_Fun_Project/shopping-bill-system.py
@@ -12,16 +12,34 @@
 items = []
 count = 0
 
+print("=== Welcome to the shop bill System ===")
+
 while True:
-    price = input("Enter the item prices ( type done ): ")
 
-    if price.lower() == "done":
+    name = input("Please enter the item name ( Or type Done ): ")
+
+    if name.lower() == "done":
         break
+    price = float(input("Enter the item prices: "))
 
-    items.append(float(price))
+    items.append((name, price))
     count += 1
 
-total = sum(items)
+ # total = sum(items)
+
+total = 0
+
+print("=== Receipt ===")
+print("=== Date ===")
+
+for item in items:
+    print(f"{item[0]} price - £{item[1]}")
+    total = total + item[1]
+
+print("=====================")
+print(f"Bill is {count} item and price is {total}")
+
+
 
 print(f"Total bill of {count} is : {total}")
 
