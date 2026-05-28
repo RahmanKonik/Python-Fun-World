47010e73aa7b39f4f083f8eb988fb642f8033113
Konik Rahman <konikrahman06@gmail.com>
2026-05-28 22:18:12 +0300

    28/5/26: create a simple car class and test it

---
+++ OOP_python_simple-car.py
@@ -0,0 +1,33 @@
+
+
+#=================================
+#date :- 28.05.2026
+#Author :- Konik Rahman
+# Email:- konikrahman06@gmail.com
+#=================================
+
+#First python program: create a simple car and test it.
+
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+    def car_show_info(self):
+        print("Brand: ", self.brand)
+        print("Color: ", self.color)
+
+car1 = Car("Toyota", "Grey")
+car2 = Car("Tesla", "Blue")
+
+car1.car_show_info()
+car2.car_show_info()
+
+
+#=================================
+print("========@@@@@=======")
+print("     Konik Rahman")
+print("========@@@@@=======")
+
+
+# ===End===
