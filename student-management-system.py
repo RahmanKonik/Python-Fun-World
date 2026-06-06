d657b685d26ab3e2065dc4d5b7c83e1f30a070e0
Konik Rahman <konikrahman06@gmail.com>
2026-06-07 01:46:01 +0300

    07-06-26: update the student grade after passing a parameter

--- student-management-system.py
+++ student-management-system.py
@@ -18,9 +18,16 @@ class Student:
     def show_student(self):
         print(f"Student details: name {self.name}, age: {self.age}, grade: {self.grade}")
 
+    def update_grade(self, new_grade):
+        self.grade = new_grade
+        print(f"{new_grade}, grade is updated")
 
-student1 = Student("Rahman", 26, "A")
 
+
+student1 = Student("Rahman", 26, "B")
+
+student1.show_student()
+student1.update_grade("A")
 student1.show_student()
 
 #=================================
